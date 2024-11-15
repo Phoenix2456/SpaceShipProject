cff6407467adba8d57bc5c0f9158e73e74a0b44c
Benny <phoenixshadow2456@gmail.com>
2024-11-15 13:27:05 +0000

    up

--- main.py
+++ main.py
@@ -104,6 +104,7 @@ def main():
                 print(f"Bay {bay['bay_id']}: {ship['ship_name']} - {ship['arrival_time']} to {ship['departure_time']}")
                 print()
                 print()
+print()
 
 
 
